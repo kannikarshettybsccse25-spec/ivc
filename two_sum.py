5376156b43bb3a1073289723d0af6fce1e494962
kannikarshettybsccse25-spec <kannikarshettybsccse25@rvu.edu.in>
2026-04-13 22:36:51 +0530

    Added Two Sum Solution

---
+++ two_sum.py
@@ -0,0 +1,17 @@
+def twoSum(nums, target):
+    num_map = {}
+
+    for i, num in enumerate(nums):
+        complement = target - num
+
+        if complement in num_map:
+            return [num_map[complement], i]
+
+        num_map[num] = i
+
+
+nums = [2,7,11,15]
+target = 9
+
+result = twoSum(nums, target)
+print(result)
